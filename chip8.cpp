e39c1d07aa70650456226ad00db1d1bf4b5fc529
nhantran1711 <traty191@mymail.unisa.edu.au>
2025-12-27 21:59:51 +1030

    adding structure for sdl window pop up, create config options for window size, height, widtg, default as center

--- chip8.cpp
+++ chip8.cpp
@@ -1,11 +1,39 @@
 
 #include <iostream>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+typedef struct 
+{
+    SDL_Window *window;
+
+} sdl_t;
+
+typedef struct 
+{
+    uint32_t window_height; // SDL Window width
+    uint32_t window_width; // SDL Window Height
+} config_t;
+
 
 // Init SDL
-bool init_sdl(void) {
+bool init_sdl(sdl_t *sdl, const config_t config) {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
         SDL_Log("Could not init SDL subsystem: %s" , SDL_GetError());
+        return false;
+    }
+
+    sdl -> window = SDL_CreateWindow(
+        "SDL 3 Window ", // Window title
+        SDL_WINDOWPOS_CENTERED, // X pos
+        SDL_WINDOWPOS_CENTERED, // Y pos
+        config.window_width, // Width, in pixel
+        config.window_height, // Height, in pixel
+        SDl_Window_OPENGL // Flags
+    )
+
+    if (!sdl -> window) {
+        SDL_Log("Could not create SDL Window %s", SDL_GetError())
         return false;
     }
     return true;
@@ -13,16 +41,24 @@ bool init_sdl(void) {
 
 // Final clean up program
 void final_cleanup(void) {
+    SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
 // Main method
 int main() {
 
+    sdl_t sdl = {0};
+    bool done = false;
+
     // Init SDL
-    if (!init_sdl()) {
+    if (!init_sdl(&sdl)) {
         exit(EXIT_FAILURE);
     }
+
+    // Init emulater configs
+    config_t config = {0};
+    
 
     // Final Cleanup
     final_cleanup();
